f42ed76151c7fc7ae3f27f3be0196818142afcff
Prathamesh <prathameshgawande100@gmail.com>
2024-07-16 17:35:47 +0530

    s2

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 // import { clerkMiddleware } from "@clerk/nextjs/server";
 import { authMiddleware } from "@clerk/nextjs/server";
 export default authMiddleware({
-  publicRoutes:["/","/api/webhooks(.*)","/:username"]
+  publicRoutes:["/","/api/webhooks(.*)","/:username","/search"]
 });
 
 export const config = {
